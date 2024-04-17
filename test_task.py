f67fad1886a2055a1be9482ff1f967c45844738f
Samael <92209224+Uncle-Samvel@users.noreply.github.com>
2024-04-17 20:48:01 +0300

    test_task

---
+++ test_task.py
@@ -0,0 +1,19 @@
+def generate_expression(result: list, total: int, current_num: int = 9, current_expression: str = '0'):
+    if current_num == 0:
+        if eval(current_expression) == total:
+            result.append(current_expression)
+        return
+
+    generate_expression(result, total, current_num - 1, str(current_num) + '+' + current_expression)
+    generate_expression(result, total, current_num - 1, str(current_num) + '-' + current_expression)
+    generate_expression(result, total, current_num - 1, str(current_num) + current_expression)
+
+
+# число которое необходимо получить
+required_number = 200
+answer = list()
+generate_expression(result=answer, total=required_number)
+
+print(f'only {len(answer)} expressions:')
+for expression in answer:
+    print(f'{expression} = {required_number}')
